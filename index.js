45ef25168fe477b356babf611d4f0df388369b40
Nisha <kumari.nisha6195@gmail.com>
2022-01-09 20:39:12 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function factorial(x) {
+    if (x == 0)
+        return 1;
+    else
+        return x * factorial(x - 1);
+}
+function fact() {
+    var n = document.getElementById("query1").value;
+    if (n == "")
+        alert("Enter any postive number")
+    else if (n >= 0) {
+        const output = factorial(n);
+        document.getElementById("result").innerText = output;
+    }
+}
